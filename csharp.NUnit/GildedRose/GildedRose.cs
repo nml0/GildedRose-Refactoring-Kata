ad1369df6bff37a17d5684e0942a9496db8b261a
Nicolas Logioia <nicolaslogioia@gmail.com>
2024-08-23 12:28:08 -0300

    Added constants for hard coding max and min quantity, simplified the quantity checks

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GildedRoseKata;
 
@@ -7,6 +9,9 @@ public class GildedRose
     IList<Item> Items;
 
     List<string> LegendaryItemsNames = new List<string> {"Sulfuras, Hand of Ragnaros"};
+
+    public const int MinQuality = 0;
+    public const int MaxQuality = 50;
 
     public GildedRose(IList<Item> Items)
     {
@@ -25,10 +30,7 @@ public class GildedRose
 
             if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (item.Quality > 0)
-                {
-                    item.Quality = item.Quality - 1;
-                }
+                item.Quality = Math.Max(MinQuality, item.Quality - 1);
             }
             else
             {
@@ -40,18 +42,12 @@ public class GildedRose
                     {
                         if (item.SellIn < 11)
                         {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
+                            item.Quality = Math.Min(MaxQuality, item.Quality + 1);
                         }
 
                         if (item.SellIn < 6)
                         {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
+                            item.Quality = Math.Min(MaxQuality, item.Quality + 1);
                         }
                     }
                 }
@@ -66,22 +62,16 @@ public class GildedRose
                 {
                     if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (item.Quality > 0)
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
+                        item.Quality = Math.Max(MinQuality, item.Quality - 1);
                     }
                     else
                     {
-                        item.Quality = item.Quality - item.Quality;
+                        item.Quality = 0;
                     }
                 }
                 else
                 {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
+                    item.Quality = Math.Min(MaxQuality, item.Quality + 1);
                 }
             }
         }
